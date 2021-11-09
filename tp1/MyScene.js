a09b78b105be36605b70b3f7d049a46869debe54
Rui Rodrigues <ruirodrig@fe.up.pt>
2021-11-09 15:32:10 +0000

    Base repository

---
+++ tp1/MyScene.js
@@ -0,0 +1,144 @@
+import { CGFscene, CGFcamera, CGFaxis, CGFappearance } from "../lib/CGF.js";
+import { MyQuad } from "./MyQuad.js";
+
+/**
+* MyScene
+* @constructor
+*/
+export class MyScene extends CGFscene {
+    constructor() {
+        super();
+    }
+    init(application) {
+        super.init(application);
+        this.initCameras();
+        this.initLights();
+        this.initMaterials();
+
+        //Background color
+        this.gl.clearColor(0.0, 0.0, 0.0, 1.0);
+
+        this.gl.clearDepth(100.0);
+        this.gl.enable(this.gl.DEPTH_TEST);
+        this.gl.enable(this.gl.CULL_FACE);
+        this.gl.depthFunc(this.gl.LEQUAL);
+
+        //Initialize scene objects
+        this.axis = new CGFaxis(this);
+        this.plane = new MyQuad(this);
+        
+        //Variables connected to MyInterface
+        this.displayAxis = true;
+        this.displayNormals = true;
+
+        this.updateNormalViz();
+
+    }
+    initLights() {
+        this.setGlobalAmbientLight(0.2, 0.2, 0.2, 1.0);
+
+        this.lights[0].setPosition(2.0, 2.0, -1.0, 1.0);
+        this.lights[0].setDiffuse(1.0, 1.0, 1.0, 1.0);
+        this.lights[0].setSpecular(1.0, 1.0, 1.0, 1.0);
+        this.lights[0].enable();
+        this.lights[0].setVisible(false);
+        this.lights[0].update();
+
+        this.lights[1].setPosition(0.0, -1.0, 2.0, 1.0);
+        this.lights[1].setDiffuse(0.5, 0.5, 0.5, 1.0);
+        this.lights[1].setSpecular(0.5, 0.5, 0.0, 1.0);
+        this.lights[1].enable();
+        this.lights[1].setVisible(false);
+        this.lights[1].update();
+    }
+    initCameras() {
+        this.camera = new CGFcamera(0.4, 0.1, 500, vec3.fromValues(10, 10, 10), vec3.fromValues(0, 0, 0));
+    }
+
+    updateCustomMaterial() {
+        this.customMaterial.setAmbient(...this.hexToRgbA(this.customMaterialValues['Ambient']));
+        this.customMaterial.setDiffuse(...this.hexToRgbA(this.customMaterialValues['Diffuse']));
+        this.customMaterial.setSpecular(...this.hexToRgbA(this.customMaterialValues['Specular']));
+        this.customMaterial.setShininess(this.customMaterialValues['Shininess']);
+    };
+
+    initMaterials() {
+
+        // Custom material (can be changed in the interface)
+        // initially midrange values on ambient, diffuse and specular, on R, G and B respectively
+
+        this.customMaterialValues = {
+            'Ambient': '#0000ff',
+            'Diffuse': '#ff0000',
+            'Specular': '#000000',
+            'Shininess': 10
+        }
+        this.customMaterial = new CGFappearance(this);
+
+        this.updateCustomMaterial();
+    }
+    display() {
+        // ---- BEGIN Background, camera and axis setup
+        // Clear image and depth buffer everytime we update the scene
+        this.gl.viewport(0, 0, this.gl.canvas.width, this.gl.canvas.height);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+
+        // Initialize Model-View matrix as identity (no transformation
+        this.updateProjectionMatrix();
+        this.loadIdentity();
+        
+        // Apply transformations corresponding to the camera position relative to the origin
+        this.applyViewMatrix();
+        
+
+        this.lights[0].update();
+        this.lights[1].update();
+
+        // Draw axis
+        if (this.displayAxis)
+            this.axis.display();
+
+        // ---- BEGIN Primitive drawing section
+
+        this.customMaterial.apply();
+
+        
+        this.plane.display();
+
+        // ---- END Primitive drawing section
+    }
+
+    updateNormalViz()
+    {
+        if (this.displayNormals) {
+            this.plane.enableNormalViz();
+        }
+        else {
+            this.plane.disableNormalViz();
+        }
+    }
+
+    hexToRgbA(hex)
+    {
+        var ret;
+        //either we receive a html/css color or a RGB vector
+        if(/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)){
+            ret=[
+                parseInt(hex.substring(1,3),16).toPrecision()/255.0,
+                parseInt(hex.substring(3,5),16).toPrecision()/255.0,
+                parseInt(hex.substring(5,7),16).toPrecision()/255.0,
+                1.0
+            ];
+        }
+        else
+            ret=[
+                hex[0].toPrecision()/255.0,
+                hex[1].toPrecision()/255.0,
+                hex[2].toPrecision()/255.0,
+                1.0
+            ];
+        return ret;
+    }
+
+
+}
---
+++ tp1/README.md
@@ -0,0 +1,23 @@
+# SGI 2022/2022 - Project 1
+
+# Group TxGy
+
+| Name             | Number    |
+| ---------------- | --------- | 
+| Jane Doe         | 201...... | 
+| John Doe         | 201...... |
+
+
+## Project Notes
+
+(Place here the main highlights and difficulties of your project)
+
+## Screenshots
+
+### 1 - Overall View
+(brief description)
+![Screenshots 1](tp1/screenshots/img1.png)
+
+### 2 - ....
+(add other screenshots here)
+

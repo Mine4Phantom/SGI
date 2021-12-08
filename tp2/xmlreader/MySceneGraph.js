113c504002eeb0b4834c5a2fee8464b661d7ce6b
Mine4Phantom <coelho.pedromiguel@gmail.com>
2021-12-08 22:18:16 +0000

    Removed some comments

--- tp2/xmlreader/MySceneGraph.js
+++ tp2/xmlreader/MySceneGraph.js
@@ -1129,11 +1129,7 @@ export class MySceneGraph {
      */
     displayScene() {
         //To do: Create display loop for transversing the scene graph
-
-        //To test the parsing/creation of the primitives, call the display function directly
-        //this.primitives['demoRectangle'].display();
-        //this.primitives['demoTriangle'].display();
-        //this.primitives['demoCylinder'].display();    
+   
         this.components['root'].display();
     }
 }

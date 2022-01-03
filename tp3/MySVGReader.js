ad91e0c4220d378f2891310acc6c935d9ee83493
dmbg1 <dmbg1999@hotmail.com>
2022-01-03 00:12:17 +0000

    Added some files

---
+++ tp3/MySVGReader.js
@@ -0,0 +1,14 @@
+import { CGFXMLreader } from '../lib/CGF.js';
+import { CGFtexture } from '../lib/CGF.js';
+
+
+/**
+ * MySVGReader class, representing the svg parser.
+ */
+export class MySVGReader {
+    /**
+     * @constructor
+     */
+    constructor(filename, scene) {
+    }
+}

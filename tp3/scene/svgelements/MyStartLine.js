65071c2c7cd6375abb51b01bb4753996acb8ff44
dmbg1 <dmbg1999@hotmail.com>
2022-01-18 23:07:57 +0000

    Start line class

--- tp3/scene/svgelements/MyStartLine.js
+++ tp3/scene/svgelements/MyStartLine.js
@@ -1,6 +1,13 @@
 export class MyStartLine {
-    constructor(scene, orientation) {
+    constructor(scene, position) {
         this.scene = scene;
-        this.orientation = orientation;
+        this.position = position;
     }
+
+    display() {
+        this.scene.pushMatrix();
+            this.scene.translate(this.position[0], 0, this.position[1]);
+            this.scene.graph.components['start_line'].display();
+        this.scene.popMatrix();
+    } 
 }
